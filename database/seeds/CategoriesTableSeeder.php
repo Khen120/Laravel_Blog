d947351977bbddfc4eba0f201bad3c2b8cd96918
Khen120 <khm1991120@gmail.com>
2020-08-27 10:16:55 +0700

    migrate error

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'user_id' => '1',
+                'name' => 'Laravel',
+                'slug' => 'Laravel',
+                'is_published' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+
+            [
+                'user_id' => '1',
+                'name' => 'WordPress',
+                'slug' => 'WordPress',
+                'is_published' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+
+            [
+                'user_id' => '1',
+                'name' => 'Python',
+                'slug' => 'Python',
+                'is_published' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+
+            [
+                'user_id' => '1',
+                'name' => 'Javascript',
+                'slug' => 'Javascript',
+                'is_published' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+
+            [
+                'user_id' => '1',
+                'name' => 'Django',
+                'slug' => 'Django',
+                'is_published' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ]);
+    }
+}
